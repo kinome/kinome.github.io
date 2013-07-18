b5f29382df368b1de005f99f1e02dcbf51aa3940
Sean Wilkinson <sean@mathbiol.org>
2013-07-18 18:10:42 -0500

    Created a static content app for Heroku

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php # source code
+
+#-  index.php ~~
+#
+#   This file only exists so that Heroku will build the app using the default
+#   Apache/PHP buildpack. Thanks to the settings in .htaccess, this file will
+#   never actually be executed as a script.
+#
+#                                                       ~~ (c) SRW, 18 Jul 2013
+#                                                   ~~ last updated 18 Jul 2013
+
+#-  vim:set syntax=php:
