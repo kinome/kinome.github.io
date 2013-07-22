353bb0635fa7c821029ea30d7e2793762f5fade8
Sean Wilkinson <sean@mathbiol.org>
2013-07-22 11:14:07 -0500

    Linting and whitespace

--- js/general/wasInline.js
+++ js/general/wasInline.js
@@ -4,21 +4,21 @@ google.load('visualization', '1', {packages: ['corechart']});
 
 //This is to get the tabs to work at the top of the toolbox
 $('#tag1').click(function (e) 
-		{
-	  	e.preventDefault();
-	  	$(this).tab('show');
-		});
-	$('#tag2').click(function (e) 
-		{
-	  	e.preventDefault();
-	  	$(this).tab('show');
-		});
-	$('#tag3').click(function (e) 
-		{
-	  	e.preventDefault();
-	  	$(this).tab('show');
-		KINOMICS.fileManager.UI.navigationBar.setDataType();
-		});
-		
+        {
+          e.preventDefault();
+          $(this).tab('show');
+        });
+    $('#tag2').click(function (e) 
+        {
+          e.preventDefault();
+          $(this).tab('show');
+        });
+    $('#tag3').click(function (e) 
+        {
+          e.preventDefault();
+          $(this).tab('show');
+        KINOMICS.fileManager.UI.navigationBar.setDataType();
+        });
+        
 
 Math.uuid = function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b};
