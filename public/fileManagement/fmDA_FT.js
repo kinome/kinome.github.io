b9d902a5c27ff7d2dc47eeb2f798e3a7c39bdbde
Alex <a.m.dussaq@gmail.com>
2014-11-03 12:07:31 -0600

    hopefully getting the list functions to work properly.

--- public/fileManagement/fmDA_FT.js
+++ public/fileManagement/fmDA_FT.js
@@ -98,9 +98,9 @@ KINOMICS.fileManager.DA.fusionTables = (function () {
             fuse.login(function (log) {
                 //TODO: handle failed login...
                 lib.loggedIn = true;
-                userName();
-                fuse.getTablesByName(configFileName, getTableListFromConfig);
-                fuse.onComplete(callback);
+                $.getJSON('https://7f3d27bba7ddcc2ee9a1f12b367e81c165b25ae7.googledrive.com/host/0Bw9j7WvO_WpORkY3WVlLRVNSb0E', function (resp) {
+                    getTableListFromConfig(resp, callback);
+                });
             });
 
             //update libraries :-) - closed for code collapsing purposes.
@@ -141,42 +141,11 @@ KINOMICS.fileManager.DA.fusionTables = (function () {
             }
         };
 
-        makeConfigFile = function () {
-            //TODO: make multiple config files so sharing becomes more logical. One for saving current changes, one for functional stuff (folders ect) and then as sharing becomes serious, add ones for sharing.
-            //variable declaration
-            var fold1, makeSubFolder, addFolderToTriples, tempTriples = [];
-            //variable definitions
-
-            //function defintions
-            addFolderToTriples = function (type, response) {
-                tempTriples.push([response.id.toString(), RDF.type, type, Math.uuid(), currentDate(), userName()]);
-                files[type] = response.id.toString();
-                // tempTriples.push([response.id.toString(), RDF.gdJSON, JSON.stringify(response), Math.uuid(), currentDate(), userName()]);
-            };
-
-            makeSubFolder = function (res) {
-                addFolderToTriples(RDF.kinomicFolder, res);
-                //Make sub folders for data, analysis, configs
-                fuse.newFolder('data', {"parents": [{id: res.id}]}, function (x) {addFolderToTriples(RDF.dataFolder, x); });
-                fuse.newFolder('config', {"parents": [{id: res.id}]}, function (x2) {
-                    addFolderToTriples(RDF.configFolder, x2);
-                    newTable(configFileName, x2.id.toString(), function (y2) {
-                        // tempTriples.push([y2.tableId, RDF.type, 'configFile', Math.uuid(), currentDate(), userName()]);
-                        currentConfig = y2.tableId;
-                        //tempTriples.push([y2.tableId, RDF.gdJSON, JSON.stringify(y2), Math.uuid(), currentDate(), userName()]);
-                        fuse.submitLinesToTable(currentConfig, tripColumns, tempTriples, function () {});
-                    });
-                });
-                fuse.newFolder('analysis', {"parents": [{id: res.id}]}, function (x) {addFolderToTriples('analysisFolder', x); });
-            };
-
-            //Make main folder
-            fuse.newFolder('autoKinomicsToolbox', {}, makeSubFolder);
-        };
-
-        getTableListFromConfig = function (tables) {
+        getTableListFromConfig = function (tables, callback) {
             //variable declarations
             var i, length, myConfig, files;
+
+            mainConfig(tables.pop());
 
             //variable definitions
             length = tables.length;
@@ -184,25 +153,11 @@ KINOMICS.fileManager.DA.fusionTables = (function () {
             files = [];
 
             //looking at an array of tables... with [0] - name, and [1] - tableID
-            if (length === 0) {
-                makeConfigFile();
-            } else {
-                for (i = 0; i < length; i += 1) {
-                    files.push(tables[i].id);
-                    if (tables[i].userPermission.role === 'owner' && (!myConfig.length || tables[i].createdDate > myConfig[1])) {
-                        myConfig = [tables[i].id, tables[i].createdDate];
+            if (length !== 0) {
+                for (i = 0; i < files.length; i += 1) {
+                    if (currentConfig !== files[i]) {
+                        addFilesToActiveList(tables[i]);
                     }
-                }
-                if (!myConfig.length) {
-                    makeConfigFile();
-                } else {
-                    currentConfig = myConfig[0];
-                    for (i = 0; i < files.length; i += 1) {
-                        if (currentConfig !== files[i]) {
-                            fuse.queryTable(files[i], {columns: tripColumns, order: tripColumns[4]}, addFilesToActiveList);
-                        }
-                    }
-                    fuse.queryTable(currentConfig, {columns: tripColumns, order: tripColumns[4]}, mainConfig);
                 }
             }
         };
@@ -309,77 +264,7 @@ KINOMICS.fileManager.DA.fusionTables = (function () {
         };
     }());
 
-    //TODO: transport most of these triples to writting from fmDA, reason: when writting triples to s3db, the triples should be the same...
-    //Write functions: writeFile, and write (triples)
-    (function () {
-        //TODO: add status updates....
-        //TODO: add file name to config file.
-        var createBarcodeFile;
-        writeFile = function (writeFileObj) {
-            //variable declarations
-            var tempTriples = [], batchID = writeFileObj.batchID, funcParams;
-            fuse.writeFile(writeFileObj.data, files[RDF.dataFolder], function (response) {
-                //TODO: deal with failed response...
-                if (response.error) {
-                    throw 'error uploading file';
-                } else {
-                    //Triples: BatchID RDF:type RDF:batch, BatchID RDF:file {}.id, {}.id RDF:type flatFile, {}.id RDF:gdJSON {}
-                    // tempTriples.push([RDF.list(batchID), RDF.type, RDF.batch, Math.uuid(), currentDate(), userName()]);
-                    // tempTriples.push([RDF.list(batchID), RDF.file, response.webContentLink, Math.uuid(), currentDate(), userName()]);
-                    // tempTriples.push([RDF.list(batchID), RDF.name, writeFileObj.data.name, Math.uuid(), currentDate(), userName()]);
-                    tempTriples.push([response.webContentLink, RDF.type, RDF.flatFile, Math.uuid(), currentDate(), userName()]);
-                    tempTriples.push([response.webContentLink, RDF.name, writeFileObj.data.name, Math.uuid(), currentDate(), userName()]);
-                    tempTriples.push([RDF.list(writeFileObj.batchID), RDF.type, RDF.batch, Math.uuid(), currentDate(), userName()]);
-                    tempTriples.push([RDF.list(writeFileObj.batchID), RDF.name, writeFileObj.data.name, Math.uuid(), currentDate(), userName()]);
-                    tempTriples.push([RDF.list(writeFileObj.batchID), RDF.file, response.webContentLink, Math.uuid(), currentDate(), userName()]);
-                    addFilesToActiveList({rows:tempTriples});
-                    fuse.submitLinesToTable(currentConfig, tripColumns, tempTriples, function (x) {
-                        writeFileObj.callback();
-                    });
-                }
-            });
-        };
-        saveBarcode = function (dataObj) {
-            var doc, tempTrips, i, writeIt, tempTriples, createTriples;
-            console.log("number 1", JSON.parse(JSON.stringify(dataObj.data)));
-            tempTriples = [];
-            //This function creates a blob from the string and sends to to google
-            writeIt = function (str, name, callback) {
-                var bb = new Blob([str], {type: "text/plain"});
-                bb.name = name;
-                fuse.writeFile(bb, files[RDF.dataFolder], 
-                    function (response) {
-                        createTriples(response); 
-                        callback();
-                    });
-            };
 
-            //This function responds to the file being written to google
-            createTriples = function (response) {
-                //TODO: Deal with error response
-                tempTriples.push([dataObj.id, RDF.type, RDF.data, Math.uuid(), currentDate(), userName()]);
-                tempTriples.push([dataObj.id, RDF.file, response.webContentLink, Math.uuid(), currentDate(), userName()]);
-                tempTriples.push([dataObj.batchID, RDF.data, dataObj.id, Math.uuid(), currentDate(), userName()]);
-                tempTriples.push([dataObj.id, RDF.name, dataObj.name, Math.uuid(), currentDate(), userName()]);
-                addFilesToActiveList({rows:tempTriples});
-                fuse.submitLinesToTable(currentConfig, tripColumns, tempTriples, function (x) { console.log('written....')});
-            };
-            writeIt(JSON.stringify(dataObj.data), dataObj.name, dataObj.callback);
-        };
-
-        addBarcodeData = function (barcodeObj) {
-            //TODO: get rid of globals
-            //TODO: deal with alerting the user of data being saved....
-            newTable('barcode_' + barcodeObj.name, files.dataFolder.id, function (res) {
-                var dataTriples, configTriples, i;
-                //TODO: get rid of blank function.
-                fuse.submitLinesToTable(res.tableId, tripColumns, [barcodeObj.JSON.stringify(barcodeObj)], function () {});
-                //fuse.submitLinesToTable(files.configFile[currentConfig].googleDriveObj.tableId, tripColumns, [
-                // [barcodeObj.batchID, RDF.triplesData, JSON.stringify(res), Math.uuid(), currentDate(), userName()]
-                // ], function () {});
-            });
-        };
-    }());
 
     //This is for listing the information avaliable
     (function () {
@@ -407,144 +292,6 @@ KINOMICS.fileManager.DA.fusionTables = (function () {
         };
     }());
 
-    //this is for loading data
-    (function () {
-        //TODO: deal with error from fusion tables call...
-        lib.loadData = function (dataObj) {
-            globFT.push(dataObj);
-            var file = bySubject[dataObj.uuid][RDF.file][0][2];
-            file = file.replace(/([\S\s]+id\=)([\S\s]+)(\&export[\S\s]+)/, "$2");
-            fuse.readFile(file, function (response) {
-                dataObj.callback(response);
-            });
-        };
-    }());
-
-
-
-
-
-
-
-
-    //Creating new tables
-    newTable = function (fileName, folder, callback) {
-        //Create table, then move it to the appropriate folder (This requires 3 requests, 1. make file, 2. add parent [add to folder], 3. remove parent [remove from root folder])
-        fuse.newTable(fileName, tripNewColumns, {}, function (res) {
-            if (folder) {
-                fuse.addFileToFolder(res.tableId, folder, function () {});
-                fuse.removeFileFromFolder(res.tableId, files.rootFolder(), function () {});
-            }
-            fuse.onComplete(function () {
-                callback(res);
-            });
-        });
-    };
-
-    saveBarcodes = function (barcodeArr, callback, uiUpdate) {
-        console.error('saveBarcodes removed');
-        return;
-        // //variable declarations
-        // var i, fuseTableID, finalCallback, lines, rows, thisCallback, uid, date, author;
-
-        // //variable definitions
-        // lines = [];
-        // author = "tester";
-        // date = (new Date()).toISOString();
-
-
-        // /*
-        // for (fuseTableID in activeFiles.barcode) {
-        //     if (activeFiles.barcode.hasOwnProperty(fuseTableID)) {
-        //         break;
-        //     }
-        // }*/
-        // fuseTableID = '1lbl5Cttwj7XIpsd6XSOJFH6lGWsQaWQw9zPKEXY';
-        // if (barcodeArr.length > 0) {
-        //     //fusionTables_barWellFileColumns = ['Barcode_Well', 'JSON', 'RDF', 'Referring Table and Row'];
-        //     for (i = 0; i < barcodeArr.length; i += 1) {
-        //         barcodeArr[i].db.barcodeFile = {file: fuseTableID};
-        //         barcodeArr[i].db.changed = false;
-        //         uid = 'ft' + fuseTableID + ":" + Math.uuid();
-        //         lines.push(
-        //             [uid, 'uabKin:hasContent', barcodeArr[i].asString('short'), date, author],
-        //             [uid, 'uabKin:hasName', barcodeArr[i].name, date, author],
-        //             [uid, 'uabKin:fileType', 'uabKin:barcodeFile', date, author],
-        //             [uid, 'uabKin:fromGroup', barcodeArr[i].db.originFile.file, date, author]
-        //         );
-        //     }
-        //     thisCallback = function (x, y) {
-        //         var i, groups, uid, toSubmit;
-        //         groups = {};
-        //         toSubmit = [];
-        //         for (i = 0; i < x.rows.length; i += 1) {
-        //             groups[lines[i][0]] = groups[lines[i][0]] || [];
-        //             groups[lines[i][0]].push(fuseTableID + '_r_' + x.rows[i][0]);
-        //         }
-        //         for (uid in groups) {
-        //             if (groups.hasOwnProperty(uid)) {
-        //                 toSubmit.push([uid, 'uabKin:fileGroup', groups[uid].join('AND'), date, author]);
-        //             }
-        //         }
-        //         fuse.submitLinesToTable(fuseTableID, tripColumns, toSubmit, function (x, y) {
-        //             var i;
-        //             for (i = 0; i < x.rows.length; i += 1) {
-        //                 barcodeArr[i].db.barcodeFile.file = x.rows[i][0]; //TODO: fix this once barcodes are saved with uuids...
-        //             }
-        //             callback(x, y);
-        //         });
-        //     };
-        //     //TODO: fix documentation in fusionTables.js for this function...
-        //     fuse.submitLinesToTable(fuseTableID, tripColumns, lines, thisCallback); //TODO: change callback...
-        // }
-        // fuse.onComplete(uiUpdate);
-    };
-
-
-    getUserName = function (callback) {
-        fuse.getUserName(callback);
-        //this is it...
-    };
-
-    fusionTablesSave = function (barcodeObj, callback) {
-        //variable declarations
-        var i, length;
-
-        //variable definitions
-        //TODO:check user input
-        callback = typeof callback === 'function' ? callback : function () {};
-        length = barcodeObj.db.barcodeFiles.length;
-
-        for (i = 0; i < length; i += 1) {
-            //TODO: add if has user permissions
-            fuse.updateTableLine(barcodeObj.db.barcodeFiles[i].file, {JSON: barcodeObj.asString()}, barcodeObj.db.barcodeFiles[i].rowID, callback);
-        }
-    };
-
-    getTableLines = function (parseObj, callback) {
-        //variable declarations
-        var category, file, fileUID, results, row, rows;
-        //variable definitions
-        results = [];
-        for (row in triples) {
-            if (triples.hasOwnProperty(row) && triples[row][1] === 'uabKin:fileGroup') {
-                results.push(makeTableLine(row, parseObj));
-            }
-        }
-        callback(results);
-    };
-
-    getTriplesWithoutContent = function (fileName) {
-        fuse.queryTable(fileName, {columns: tripColumns.concat(['ROWID']), where: "'Predicate' DOES NOT CONTAIN 'hasContent'"}, function (x) {
-            var i, rowID;
-            for (i = 0; i < x.rows.length; i += 1) {
-                rowID = fileName + '_r_' + x.rows[i].pop();
-                triples[rowID] = x.rows[i];
-            }
-        });
-    };
-
-    
 
     currentDate = function () {
         return (new Date()).toISOString();
