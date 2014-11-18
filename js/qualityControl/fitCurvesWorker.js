34a940b26ee48a882226f55213a55f13616b6dfb
Alex <a.m.dussaq@gmail.com>
2014-11-17 21:42:14 -0600

    Add in the dictionary for the binomial distribution

--- js/qualityControl/fitCurvesWorker.js
+++ js/qualityControl/fitCurvesWorker.js
@@ -1,5 +1,5 @@
 /*global self: false */
-/*jslint evil:true, todo: true */
+/*jslint evil: true, todo: true */
 
 // TODO: check user input...
 //Container for all code. Will be run on load
@@ -7,7 +7,7 @@
     'use strict';
 
     //variable declarations
-    var fmincon, determineRunningConditions;
+    var fmincon, determineRunningConditions, binomialDict;
 
     //variable definitions
 
@@ -105,13 +105,20 @@
                 }
             }
 
-            return binomialProb(tot, 0.5, [0, count]);
+            return binomialProb(tot, count);
         };
 
-        binomialProb = function (n, prob, range) {
-            var i, sol = 0;
-            for (i = range[0]; i <= range[1]; i += 1) {
-                sol += combine(n, i) * Math.pow(prob, i) * Math.pow(1 - prob, n - i);
+        binomialProb = function (n, end) {
+            var i, sol = 0, prob = 0.5; // Since this is my measure of goodness of fit, I set prob=0.5.
+
+            //If it is predefined (should be for all in the standard data set)
+            if (binomialDict.hasOwnProperty[n] && binomialDict[n].hasOwnProperty[i]) {
+                return binomialDict[n][i];
+            }
+
+            //Else actually calculate it.
+            for (i = 0; i <= end; i += 1) {
+                sol += combine(n, i) * Math.pow(prob, n);
             }
             return sol;
         };
@@ -175,4 +182,9 @@
         //return result
         self.postMessage([event.data[0], result]);
     };
+
+
+    //This is the binomial presolved dictionary based on p=0.5 from n->i
+    binomialDict = {"0": {"0": 1}, "1": {"0": 0.5, "1": 1}, "2": {"0": 0.25, "1": 0.75, "2": 1}, "3": {"0": 0.125, "1": 0.5, "2": 0.875, "3": 1}, "4": {"0": 0.0625, "1": 0.3125, "2": 0.6875, "3": 0.9375, "4": 1}, "5": {"0": 0.03125, "1": 0.1875, "2": 0.5, "3": 0.8125, "4": 0.96875, "5": 1}, "6": {"0": 0.015625, "1": 0.109375, "2": 0.34375, "3": 0.65625, "4": 0.890625, "5": 0.984375, "6": 1}, "7": {"0": 0.0078125, "1": 0.0625, "2": 0.2265625, "3": 0.5, "4": 0.7734375, "5": 0.9375, "6": 0.9921875, "7": 1}, "8": {"0": 0.00390625, "1": 0.03515625, "2": 0.14453125, "3": 0.36328125, "4": 0.63671875, "5": 0.85546875, "6": 0.96484375, "7": 0.99609375, "8": 1}, "9": {"0": 0.001953125, "1": 0.01953125, "2": 0.08984375, "3": 0.25390625, "4": 0.5, "5": 0.74609375, "6": 0.91015625, "7": 0.98046875, "8": 0.998046875, "9": 1}, "10": {"0": 0.0009765625, "1": 0.0107421875, "2": 0.0546875, "3": 0.171875, "4": 0.376953125, "5": 0.623046875, "6": 0.828125, "7": 0.9453125, "8": 0.9892578125, "9": 0.9990234375, "10": 1}, "11": {"0": 0.00048828125, "1": 0.005859375, "2": 0.03271484375, "3": 0.11328125, "4": 0.2744140625, "5": 0.5, "6": 0.7255859375, "7": 0.88671875, "8": 0.96728515625, "9": 0.994140625, "10": 0.99951171875, "11": 1}, "12": {"0": 0.000244140625, "1": 0.003173828125, "2": 0.019287109375, "3": 0.072998046875, "4": 0.19384765625, "5": 0.38720703125, "6": 0.61279296875, "7": 0.80615234375, "8": 0.927001953125, "9": 0.980712890625, "10": 0.996826171875, "11": 0.999755859375, "12": 1}};
+
 }());
